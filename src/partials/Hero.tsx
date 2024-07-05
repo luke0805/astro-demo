23663e046712884c47dfb5b52c98b579550264b9
look <luke12321@163.com>
2024-07-05 18:06:18 +0800

    astro demo

---
+++ src/partials/Hero.tsx
@@ -0,0 +1,64 @@
+import {
+  GradientText,
+  HeroAvatar,
+  HeroSocial,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          你好，我是 <GradientText>鲁柯</GradientText> 👋
+        </>
+      }
+      description={
+        <>
+          我们正在使用Astro创建网站，它{' '}
+          <a className="text-cyan-400 hover:underline" href="/">
+          非常简单
+          </a>{' '}
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-64"
+          src="/assets/images/avatar.svg"
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/twitter-icon.png"
+              alt="Twitter icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/facebook-icon.png"
+              alt="Facebook icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/linkedin-icon.png"
+              alt="Linkedin icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/youtube-icon.png"
+              alt="Youtube icon"
+            />
+          </a>
+        </>
+      }
+    />
+  </Section>
+);
+
+export { Hero };
